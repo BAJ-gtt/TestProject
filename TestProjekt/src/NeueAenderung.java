a1af24f91aac6b9a09d5220d203a25c77f7f2ad5
baj <baj@gtt-online.de>
2019-12-09 12:59:45 +0100

    Tryed to fix my misstake

---
+++ TestProjekt/src/NeueAenderung.java
@@ -0,0 +1,10 @@
+
+public class NeueAenderung {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo 2");
+		System.out.println("Hallo");
+
+	}
+
+}
---
+++ TestProjekt/src/ObviousChange.java
@@ -0,0 +1,11 @@
+
+public class ObviousChange {
+
+	public static void main(String[] args) {
+		System.out.println("Hallo");
+		System.out.println("Hallo2");
+		
+
+	}
+
+}
