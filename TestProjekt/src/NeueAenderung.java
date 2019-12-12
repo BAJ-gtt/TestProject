2d18476cdb61fef49b39696199c17e5f1c43a90d
baj <baj@gtt-online.de>
2019-12-12 09:36:45 +0100

    Error solved

--- TestProjekt/src/NeueAenderung.java
+++ TestProjekt/src/NeueAenderung.java
@@ -2,11 +2,8 @@
 public class NeueAenderung {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		boolean a;
-=======
-		String a;
->>>>>>> branch 'master' of https://github.com/BAJ-gtt/TestProject.git
+		String b;
 		//Dies löst einen Fehler aus
 		System.out.println("Hallo");
 		
