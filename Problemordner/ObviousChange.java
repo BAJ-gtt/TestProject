baaf40bb82378f4dee7d3f33bf89f298e463a187
baj <baj@gtt-online.de>
2020-02-04 13:00:58 +0100

    Update 1.0

--- Problemordner/ObviousChange.java
+++ Problemordner/ObviousChange.java
@@ -2,7 +2,9 @@
 public class ObviousChange { 
 
 	public static void main(String[] args) {
+		// Neue erste Zeile
 		System.out.println("Hallo");
 		//Merge Error Versuch
+		//TEst123
 	}
 }
--- TestProjekt/src/NeueAenderung.java
+++ TestProjekt/src/NeueAenderung.java
@@ -3,6 +3,7 @@ public class NeueAenderung {
 
 	public static void main(String[] args) {
 		boolean a;
+		String b;
 		//Dies löst einen Fehler aus
 		System.out.println("Hallo");
 		
